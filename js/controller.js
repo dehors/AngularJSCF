0dfe415366a15d0fef7ef28fea22ebb6e280d1b2
slam24 <luismartinez242011@gmail.com>
2016-02-08 18:27:33 -0600

    feat: Two way data binding & ng-model(ng-repeat,ngclick)

--- js/controller.js
+++ js/controller.js
@@ -2,4 +2,20 @@ var app = angular.module("MyFirstapp", []);
 
 app.controller("FirstController", function($scope){
 	$scope.nombre = "Luis";
+	$scope.newcomment = {};
+	$scope.comentarios = [
+	{
+		comentario: "Buen Tutorial",
+		username: "Luis"
+	},
+	{
+		comentario: "Buen Tutorial",
+		username: "Alfredo"
+	}
+	];
+	$scope.addComment = function(){
+		$scope.comentarios.push($scope.newcomment);
+		$scope.newcomment = {};
+	};
 });
+
